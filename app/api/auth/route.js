66d9cd66728720bd5539e15d8ac50a0fabd7dcc2
Christopher Tekvi <christophertekvi@gmail.com>
2026-07-14 15:19:09 +0700

    feat: set admin session expiration to 3 hours

--- app/api/auth/route.js
+++ app/api/auth/route.js
@@ -19,7 +19,7 @@ export async function POST(req) {
     httpOnly: true,
     sameSite: 'lax',
     secure: process.env.NODE_ENV === 'production',
-    maxAge: 60 * 60 * 24 * 7,
+    maxAge: 60 * 60 * 3, // 3 hours (3 * 3600 seconds)
     path: '/',
   })
 
